c3ef3b6122939e975a0b867d3db69d49c44c6626
Mickey Martinez <mickey@echobind.com>
2022-05-06 12:54:18 -0400

    Create Menu Items screen

--- src/screens/RestaurantDetailsScreen/index.js
+++ src/screens/RestaurantDetailsScreen/index.js
@@ -1,58 +1,29 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import restaurants from "../../../assets/data/restaurants.json";
+import DishListItem from "../../components/DishListItem";
+import Header from "./Header";
+import styles from "./styles";
 
 const restaurant = restaurants[0];
 
 const RestaurantDetailsScreen = () => {
   return (
     <View style={styles.page}>
-      <Image source={{ uri: restaurant.image }} style={styles.image} />
-
+      <FlatList
+        ListHeaderComponent={() => <Header restaurant={restaurant} />}
+        data={restaurant.dishes}
+        renderItem={({ item }) => <DishListItem dish={item} />}
+      />
       <Ionicons
         name="arrow-back-circle"
         size={45}
         color="white"
         style={styles.iconContainer}
       />
-
-      <View style={styles.container}>
-        <Text style={styles.title}>{restaurant.name}</Text>
-        <Text style={styles.subtitle}>
-          $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
-          {restaurant.maxDeliveryTime} minutes
-        </Text>
-      </View>
     </View>
   );
 };
 
 export default RestaurantDetailsScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    margin: 10,
-  },
-  iconContainer: {
-    position: "absolute",
-    top: 40,
-    left: 15,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 5 / 3,
-  },
-  page: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 35,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  subtitle: {
-    color: "#525252",
-    fontSize: 15,
-  },
-});
