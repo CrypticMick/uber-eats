08def2f1d5d2f22d4bb765a37ad960030d3eee87
Mickey Martinez <mickey@echobind.com>
2022-05-11 17:34:53 -0700

    React navigation set up and data sharing with useRoute

--- src/screens/RestaurantDetailsScreen/index.js
+++ src/screens/RestaurantDetailsScreen/index.js
@@ -5,16 +5,23 @@ import restaurants from "../../../assets/data/restaurants.json";
 import DishListItem from "../../components/DishListItem";
 import Header from "./Header";
 import styles from "./styles";
+import { useRoute } from "@react-navigation/native";
 
 const restaurant = restaurants[0];
 
 const RestaurantDetailsScreen = () => {
+  const route = useRoute();
+
+  const id = route.params?.id;
+  console.log(id);
+
   return (
     <View style={styles.page}>
       <FlatList
         ListHeaderComponent={() => <Header restaurant={restaurant} />}
         data={restaurant.dishes}
         renderItem={({ item }) => <DishListItem dish={item} />}
+        keyExtractor={(item) => item.name}
       />
       <Ionicons
         name="arrow-back-circle"
